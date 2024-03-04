4c24d7a61ebd22fc874c23558c76d7d03f789725
voynow <voynow99@gmail.com>
2024-03-04 08:23:55 -0500

    Better error messages

--- src/predict.py
+++ src/predict.py
@@ -30,13 +30,16 @@ def predict(
 
 def download_model_from_s3(file_name) -> None:
     """Assuming model exists in S3 model registry"""
-    s3_client = boto3.client("s3")
-    s3_client.download_file(
-        Bucket=BUCKET_NAME,
-        Key=os.path.join(PREFIX, os.path.basename(file_name)),
-        Filename=file_name,
-    )
-    print(f"File {file_name} downloaded from s3://{BUCKET_NAME}/{PREFIX}.")
+    try:
+        s3_client = boto3.client("s3")
+        s3_client.download_file(
+            Bucket=BUCKET_NAME,
+            Key=os.path.join(PREFIX, os.path.basename(file_name)),
+            Filename=file_name,
+        )
+        print(f"File {file_name} downloaded from s3://{BUCKET_NAME}/{PREFIX}.")
+    except Exception as e:
+        raise Exception(f"Error downloading model from S3: {e}")
 
 
 def initialize_model() -> DistilBertForSequenceClassification:
